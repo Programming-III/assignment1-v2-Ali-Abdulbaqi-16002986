e9f189ca8a4109420d208cbbe6ef1cd7da824fd5
Ali-Abdulbaqi-16002986 <aliazemo2006@gmail.com>
2025-11-11 09:10:53 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,18 @@
 #ifndef COURSE_H
 #define COURSE_H
-
+#include <iostream>
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Course{
+private:
+string courseCode;
+string courseName;
+int maxStudents;
+Student* students;
+int currentStudents;
+public:
+void addStudent(const Student& s);
+void dispayCourseInfo();
+};
 #endif
